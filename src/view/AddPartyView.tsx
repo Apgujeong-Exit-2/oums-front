cf4bb9c0b6dde507593ca3ee1b475a0d23f3edd5
jaeho0613 <ywm2004@gmail.com>
2022-07-12 17:55:40 +0900

    [ 파티 만들기 페이지 ] - ui 작업 중간

--- src/view/AddPartyView.tsx
+++ src/view/AddPartyView.tsx
@@ -1,8 +1,34 @@
 import css from './AddPartyView.module.css';
+import { Accordion, Container } from 'react-bootstrap';
 const addPartyView = () => {
   return (
     <div className={css.root}>
-      <h2>파티 찾기 페이지 2</h2>
+      <Container className={css.container}>
+        <Accordion defaultActiveKey={['0']}>
+          <Accordion.Item eventKey='0'>
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey='1'>
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </Container>
     </div>
   );
 };
